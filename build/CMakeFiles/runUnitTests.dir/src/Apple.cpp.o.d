d8450f8142eedc9cbd88c50037dfb35a040852bb
tom <tom.m.rose@gmail.com>
2024-07-02 18:42:32 -0700

    more implementing of .gitignore

--- build/CMakeFiles/runUnitTests.dir/src/Apple.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/runUnitTests.dir/src/Apple.cpp.o: \
- /home/tom/snake/src/Apple.cpp /usr/include/stdc-predef.h
--- build/CMakeFiles/snake.dir/src/Apple.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/snake.dir/src/Apple.cpp.o: /home/tom/snake/src/Apple.cpp \
- /usr/include/stdc-predef.h
