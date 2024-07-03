716a211ad37777e394c6c8269bdd8bfaf3468860
tom <tom.m.rose@gmail.com>
2024-07-02 18:46:20 -0700

    more implementing of .gitignore

--- build/CMakeFiles/runUnitTests.dir/tests/MockNotifyable.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/runUnitTests.dir/tests/MockNotifyable.cpp.o: \
- /home/tom/snake/tests/MockNotifyable.cpp /usr/include/stdc-predef.h \
- /home/tom/snake/include/INotifyable.h \
- /home/tom/snake/include/MockNotifyable.h \
- /home/tom/snake/include/INotifyable.h
--- build/CMakeFiles/snake.dir/src/PositionManager.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/snake.dir/src/PositionManager.cpp.o: \
- /home/tom/snake/src/PositionManager.cpp /usr/include/stdc-predef.h \
- /home/tom/snake/include/PositionManager.h /home/tom/snake/include/Pos.h
